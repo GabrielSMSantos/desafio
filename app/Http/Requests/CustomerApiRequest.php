c7ab39c4bd06aabcdcbd6648331b885e9505f020
GabrielSMSantos <gabrielsaomartin@hotmail.com>
2024-07-17 13:34:53 -0300

    added customer api form request validation

---
+++ app/Http/Requests/CustomerApiRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'cpf'   => [
+                        'required',
+                        isset($this->request->all()["id"]) ? Rule::unique('customers')->ignore($this->request->all()["id"]) : 'unique:customers',
+                        'cpf'
+                    ],
+            'name'       => 'required',
+            'lastname'   => 'required',
+            'birth_date' => 'required|date|before_or_equal:2009-12-31',
+            'email'      => 'required|email:rfc,dns',
+            'gender'     => 'required|starts_with:M,F'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'cpf.required' => 'Este campo é obrigatório!',
+            'cpf.unique' => 'CPF já cadastrado!',
+            'cpf.cpf' => 'Informe um CPF válido!',
+
+            'name.required' => 'Este campo é obrigatório!',
+            'lastname.required' => 'Este campo é obrigatório!',
+
+            'birth_date.required' => 'Este campo é obrigatório!',
+            'birth_date.date' => 'Informe uma data válida!',
+            'birth_date.before_or_equal' => 'Informe uma data que seja menor ou igual a 31/12/2009',
+
+            'email.required' => 'Este campo é obrigatório!',
+            'email.email' => 'Informe um E-mail válido!',
+            'gender.required' => 'Este campo é obrigatório!',
+            'gender.starts_with' => 'Informe um Gênero válido M ou F!',
+        ];
+    }
+}
