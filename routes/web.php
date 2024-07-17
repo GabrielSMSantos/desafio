7fd093bd52d5a082b7a269c19b3f46e2f9e1b747
GabrielSMSantos <gabrielsaomartin@hotmail.com>
2024-07-17 09:14:08 -0300

    added customer routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(CustomerController::class)->name('customer.')->group(function() {
+    Route::get('/', 'index')->name('index');
+    Route::get('/novo', 'create')->name('create');
+    Route::post('/', 'store')->name('store');
+    Route::get('/{customer}', 'edit')->where('customer', '[0-9]+')->name('edit');
+    Route::put('/{customer}', 'update')->where('customer', '[0-9]+')->name('update');
+    Route::delete('/{customer}', 'delete')->where('customer', '[0-9]+')->name('delete');
 });
+
